4cabe125637ab8aa1e1aacf1624092a7d89a88c8
jctamayo <tamayojuan754@gmail.com>
2026-04-07 14:52:12 -0500

    chore: limpieza de estructura anterior del proyecto Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { RegistroComponent } from './components/registro/registro.component';
+import { BusquedaVuelosComponent } from './components/busqueda-vuelos/busqueda-vuelos.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './components/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: LoginComponent,
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'busqueda-vuelos', component: BusquedaVuelosComponent, canActivate: [AuthGuard]
+  },
+  {
+    path: 'registro', component: RegistroComponent
+  },
+];
