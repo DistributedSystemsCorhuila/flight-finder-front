ea15309eddc1346e26335269afe778158fb71763
jctamayo <tamayojuan754@gmail.com>
2026-04-08 13:06:53 -0500

    feat(HU-36): ajustes en rutas y estilos de login y registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { RegistroComponent } from './components/registro/registro.component';
 import { BusquedaVuelosComponent } from './components/busqueda-vuelos/busqueda-vuelos.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './components/guards/auth.guard';
 
 export const routes: Routes = [
   {
-    path: '', component: LoginComponent,
+    path: '', component: HomeComponent,
+  },
+  {
+    path: 'home', component: HomeComponent
   },
   {
     path: 'login', component: LoginComponent
